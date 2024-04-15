55bed389bbabf446d454d1d1fb8466e02cf7fec5
Rafael Valdés <147006865+elpro2813@users.noreply.github.com>
2024-04-15 16:47:12 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+}
+
+function modelLoaded() {
+
+}
+
+function gotPoses(results)
+{
+ 
+  
+}
+
+function draw() {
+}
+
+function take_snapshot(){    
+ save('myFilterImage.png');
+}
